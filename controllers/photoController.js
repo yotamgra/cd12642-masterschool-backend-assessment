580dd617d99fd78dcfb33092fb468a3af305e7f1
Yotam Granada <yotam.granada@gmail.com>
2023-02-11 18:06:22 +0200

    Part III - Advanced Routes: Return Combined Data

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,2 +1,69 @@
 //Require axios to make API calls
 const axios = require("axios");
+
+const API = "https://api.unsplash.com/";
+
+//@desc    Get raw Unsplash photo URLs
+//@route   GET /api/phtos
+//@access  Public
+const getRawPhotosURLs = async (req, res) => {
+  try {
+    console.log("try");
+    const response = await axios.get(
+      `${API}photos/?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const photos = response.data;
+    const rawUnsplashPhotoURLs = photos.map((photo) => photo.urls.raw);
+    res.status(200).send(rawUnsplashPhotoURLs);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error. Please try again later.",
+    });
+  }
+};
+
+//@desc    Get  Unsplash photo object by id
+//@route   GET /api/phtos/:id
+//@access  Public
+const getPhotoById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await axios.get(
+      `${API}photos/${id}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const photo = response.data;
+
+    res.status(200).send(photo);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error. Please try again later.",
+    });
+  }
+};
+
+//@desc    Get  Unsplash photo object by id
+//@route   GET /api/phtos/:id
+//@access  Public
+
+const getUsersPhotosByUsername = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const response = await axios.get(
+      `${API}users/${username}/photos?client_i=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const usersPhotos = response.data;
+
+    res.status(200).send(usersPhotos);
+  } catch (error) {
+    const statusCode = error.response.status;
+    const errorMessage = error.message;
+
+    res.status(statusCode).json({ message: errorMessage });
+  }
+};
+
+module.exports = {
+  getRawPhotosURLs,
+  getPhotoById,
+  getUsersPhotosByUsername,
+};
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -2,38 +2,18 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-const API = "https://api.unsplash.com/photos/";
+const {
+  getRawPhotosURLs,
+  getPhotoById,
+  getUsersPhotosByUsername,
+} = require("../controllers/photoController");
 
-router.route("/").get(async (req, res) => {
-  try {
-    const response = await axios.get(
-      `${API}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
-    );
-    const photos = response.data;
-    const rawUnsplashPhotoURLs = photos.map((photo) => photo.urls.raw);
-    res.status(200).send(rawUnsplashPhotoURLs);
-  } catch (error) {
-    res.status(500).json({
-      message: "Server error. Please try again later.",
-    });
-  }
-});
+router.get("/", getRawPhotosURLs);
 
-router.route("/:id").get(async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await axios.get(
-      `${API}/${id}?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
-    );
-    const photo = response.data;
+router.get("/:id", getPhotoById);
 
-    res.status(200).send(photo);
-  } catch (error) {
-    res.status(500).json({
-      message: "Server error. Please try again later.",
-    });
-  }
-});
+router.get("/user/:username", getUsersPhotosByUsername);
 
 module.exports = router;
+
 
